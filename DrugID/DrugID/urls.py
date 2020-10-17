1b39389bd5d64b37ab0601d4371d5fac33dc6c1e
JMathiszig-Lee <jakob@mathisziglee.co.uk>
2020-10-17 16:32:37 +0100

    stuff

--- DrugID/DrugID/urls.py
+++ DrugID/DrugID/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+from DrugID_API import views
+
+router = routers.DefaultRouter()
+router.register(r'results', views.ResultsViewSet)
+#router.register(r'getdrugs', views.DrugSet.as_view(), "DrugSet")
+
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('getdrugs/', views.DrugSet.as_view()),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
--- DrugID/DrugID_API/views.py
+++ DrugID/DrugID_API/views.py
@@ -1,21 +1,47 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
 from rest_framework import viewsets
-from models import Asset, 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import permissions
+
+from DrugID_API.models import Asset, Drug, Set, Result
+from DrugID_API.serializers import ResultSerializer
 
 import os
 import random
 
 # Create your views here.
-
-class DrugSetViewSet(viewsets.ReadOnlyModelViewSet):
+@permission_classes((permissions.AllowAny,))
+class DrugSet(APIView):
     #basic view to display list of drugs
 
-    #get number of drugs to return but default is 8
-    numdrugs =int(os.getenv('NUMBER_DRUGS', 8))
 
-    #select n random drugs
-    #TODO this method is potentially slow and may need updating
-    drugs = Asset.objects.all().order_by('?').filter(Group=2)[:numdrugs]
+    def get(self, request, format=None):
+        #get number of drugs to return but default is 8
+        numdrugs =int(os.getenv('NUMBER_DRUGS', 8))
+        limit =int(os.getenv('TIME_LIMIT', 3000))
+        #select n random drugs
+        #TODO this method is potentially slow and may need updating
+        drugs = Asset.objects.all().order_by('?').filter(group=2)[:numdrugs]
+        drugslist = []
+        for i in drugs:
+            drugslist.append(i)
+        #target = random.choice(drugs)
+        target = "adrenaline"
+        payload = {
+            "set_id": "fixed",
+            "ampoules": drugslist,
+            "target_drug": target,
+            "time_limit": limit
+        }
+        #save as a set
+        return Response(payload)
 
-    target = random.choice(drugs)
+class ResultsViewSet(viewsets.ModelViewSet):
+    """
+    Simple results viewset
+    """
+
+    queryset = Result.objects.all()
+    serializer_class = ResultSerializer
