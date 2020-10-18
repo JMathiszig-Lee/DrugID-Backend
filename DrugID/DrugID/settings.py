dce3d0b3070c8e502de733416f0cd8c2e58aa5da
JMathiszig-Lee <jakob@mathisziglee.co.uk>
2020-10-18 10:29:27 +0100

    trying to fix heroku

--- DrugID/DrugID/settings.py
+++ DrugID/DrugID/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import django_heroku
 
 from pathlib import Path
 
@@ -129,3 +130,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
