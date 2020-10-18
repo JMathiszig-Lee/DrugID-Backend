e5e16e89eb1c0ccce35a67fadf26fcb3fd701819
Phosphor565 <65199319+Phosphor565@users.noreply.github.com>
2020-10-18 09:12:00 +0100

    Update insert_assets_mk2.py

--- DrugID/DrugID_API/management/commands/insert_assets_mk2.py
+++ DrugID/DrugID_API/management/commands/insert_assets_mk2.py
@@ -4,16 +4,24 @@ from django.core.management.base import BaseCommand
 from DrugID_API.models import Asset, Drug
 
 import os
-class Command(BaseCommand):
 
+
+class Command(BaseCommand):
     label_types = ["industry", "iso", "blackwhite"]
 
     label_array = {}
 
-    # loop through 3 types
+    # Populates the Drug table
+    names_drug = os.listdir(Path("DrugID_API/assets/industry"))
+    for name in names_drug:
+        nm = name[:-4]
+        data_input_drug = Drug(name=nm)
+        data_input_drug.save()
+
+    # loop through 3 types populating the Asset table 
     for type in label_types:
-        # Directory for each type
-        label_type_directory = "DrugID_API/assets/"
+        # Directory for each type > 'DrugID_API/assets/' for release > '../../assets/' for debugging
+        label_type_directory = "DrugID_API/assets/" + type + "/"
 
         # put all assets names for type in array
         label_array = os.listdir(Path(label_type_directory))
@@ -22,28 +30,23 @@ class Command(BaseCommand):
         for name_label in label_array:
             # Shortens label name to remove file extension and inserts data into Drug table
             n = name_label[:-4]
-            data_input_drug = Drug(name = n)
-            data_input_drug.save()
-
-            drug = Drug(name = n)
+            # print(n) debugging
+            # Selects data from last entry made in Drug table
+            drug = Drug.objects.get(name=n)
             print(drug.id)
 
             # creates directory for database
             path = "assets/" + type + "/" + name_label
+            # print(path) debugging
+
             # creates code depending on the label type
-            if(type == "industry" ):
+            if type == "industry":
                 group_id = 1
-            elif(type == "iso"):
+            elif type == "iso":
                 group_id = 2
-            elif(type == "backwhite"):
+            elif type == "backwhite":
                 group_id = 3
 
             # Inserts Data into table
-            data_input_asset = Asset(drug_id = drug.id, asset_url = path, group = group_id)
+            data_input_asset = Asset(drug_id=drug, asset_url=path, group=group_id)
             data_input_asset.save()
-
-
-
-
-
-
